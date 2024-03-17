a1eb33f6e0a6d066a4ad597c0ad28a538a46b4f3
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2024-03-17 00:25:48 -0400

    Shhhhhhhh

---
+++ source/addons/Event.hx
@@ -0,0 +1,15 @@
+class Event extends MyAsshole
+{
+    override public function new(name:String)
+    {
+        super();
+    }
+}
+
+class MyAsshole // :troll:
+{
+    public function new()
+    {
+        throw "This isnt finished yet!! expect it in the near future tho";
+    }
+}
