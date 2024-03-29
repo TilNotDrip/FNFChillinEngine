8973ace89a2dd85779679af2abf1bb39ade426f3
Til Tjardes <til.tjardes@gmail.com>
2024-03-29 17:31:12 -0400

    wtf

--- source/addons/SongEvent.hx
+++ source/addons/SongEvent.hx
@@ -2,22 +2,36 @@ typedef SwagEvent =
 {
     var name:String;
     var params:Array<String>;
+<<<<<<< Updated upstream:source/addons/SongEvent.hx
     var type:SongEventType;
     var time:String;
 }
 
 class SongEvent extends MyAsshole
+=======
+    var strumTime:String;
+}
+
+class Event
+>>>>>>> Stashed changes:source/addons/Event.hx
 {
-    override public function new(name:String)
+    public function new(name:String, params:Array<String>)
     {
-        super();
+        updateEventData(name, params);
+    }
+
+    public var needsParams:Bool = true;
+    public var eventTypes:Array<EventType>;
+    public function updateEventData(name:String, parameters:Array<String>)
+    {
+
     }
 }
 
-class MyAsshole // :troll:
+enum abstract EventType(String)
 {
-    public function new()
-    {
-        throw "This isnt finished yet!! expect it in the near future tho";
-    }
+	var X    = 'list';
+	var Y    = 0x10;
+	var XY   = 0x11;
+	var NONE = 0x00;
 }
