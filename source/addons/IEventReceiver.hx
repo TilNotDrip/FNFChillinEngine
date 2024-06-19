f06c631dea2cb4c6ed3fbefb4f27193523a03423
TechnikTil <89487150+TechnikTil@users.noreply.github.com>
2024-06-19 10:26:35 -0600

    IEventReceiver

---
+++ source/addons/IEventReceiver.hx
@@ -0,0 +1,16 @@
+package addons;
+
+interface IEventReceiver
+{
+    public function create():Void;
+    public function update(elapsed:Float):Void;
+
+    public function cameraMovement(char:Character):Void;
+
+    public function stepHit():Void;
+    public function beatHit():Void;
+    public function sectionHit():Void;
+
+    public function startCountdown():Dynamic;
+    public function endSong():Dynamic;
+}
