301e316e7fe30488aa1c4f9e4b69d6f8bc9931a7
Til Tjardes <til.tjardes@gmail.com>
2024-06-20 19:00:55 -0600

    wip sustain

--- source/objects/game/SustainNote.hx
+++ source/objects/game/SustainNote.hx
@@ -8,19 +8,19 @@ class SustainNote extends FlxSprite
 {
     private var rgbShader:RGBShader = new RGBShader();
 
-    public function new(length:Float)
+    public function new(length:Float, ?speed:Float = 1)
     {
         var daSprite:BitmapData = null;
 
         var holdSprite:BitmapData = BitmapData.fromFile(Paths.getPath('images/ui/notes/hold.png', IMAGE, null));
 
         var daCalculatedLength:Float = length / Conductor.stepCrochet;
+        var daCalculatedHeight:Float = (44 * Math.floor(daCalculatedLength)-1) * Conductor.stepCrochet / 100 * 1.5 * speed;
+        daSprite = new BitmapData(49, Std.int(daCalculatedHeight), true, 0x0);
         
-        daSprite = new BitmapData(49, Math.floor(daCalculatedLength), true, 0x0);
-        
-        for (susNote in 0...Math.floor(daCalculatedLength))
+        for (susNote in 0...Std.int(daCalculatedHeight))
 		{
-            daSprite.draw(holdSprite, new openfl.geom.Matrix(1, 1, 1, 1, 0, susNote));
+            daSprite.draw(holdSprite, new openfl.geom.Matrix(1, 0, 0, 1, 0, susNote));
         }
 
         //Math.floor(daCalculatedLength)
