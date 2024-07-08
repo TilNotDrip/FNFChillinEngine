85a1973831212a0269bdb308ab80d5390c92745a
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2024-07-07 23:27:20 -0400

    bye bye chillin ease

--- source/addons/ChillinEase.hx
+++
@@ -1,9 +0,0 @@
-package addons;
-
-class ChillinEase extends FlxEase
-{
-	public static inline function lerp(t:Float):Float
-	{
-		return CoolUtil.coolLerp(t, 1, 0.04);
-	}
-}
--- source/import.hx
+++ source/import.hx
@@ -1,50 +1,33 @@
 // HEY!! Before adding something here, if you use the modding api, add it to the list in HScript.hx!
-
 // Stuff from Haxelibs
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
-
 import flixel.graphics.frames.FlxAtlasFrames;
-
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.group.FlxGroup;
-
 import flixel.math.FlxMath;
-
 import flixel.sound.FlxSound;
-
 import flixel.text.FlxText;
-
 import flixel.tweens.FlxEase;
-import addons.ChillinEase;
 import flixel.tweens.FlxTween;
-
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-
 import lime.app.Application;
-
 // Stuff from Chillin' Engine/Base Friday Night Funkin' itself
 import addons.*;
 import addons.Controls;
 import addons.CoolUtil;
-
 #if MOD_SUPPORT
 import modding.*;
 #end
-
 import objects.*;
-
 import options.ChillSettings;
-
 import states.game.GameBackend;
 import states.game.PlayState;
-
 import states.menus.StoryMenuState;
 import states.menus.FreeplayState;
 import states.menus.MainMenuState;
-
 import states.LoadingState;
 
 // Using
