984412e77a94ba75088add49b97047e6b921d3ae
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2024-10-23 19:03:45 -0400

    nah i'd enum

--- src/funkin/util/Controls.hx
+++ src/funkin/util/Controls.hx
@@ -30,42 +30,42 @@ class Controls
 	public var UI_UP_P(get, never):Bool;
 
 	inline function get_UI_UP_P()
-		return checkKey('UI_UP', 'just');
+		return checkKey('UI_UP', JUST_PRESSED);
 
 	public var UI_LEFT_P(get, never):Bool;
 
 	inline function get_UI_LEFT_P()
-		return checkKey('UI_LEFT', 'just');
+		return checkKey('UI_LEFT', JUST_PRESSED);
 
 	public var UI_RIGHT_P(get, never):Bool;
 
 	inline function get_UI_RIGHT_P()
-		return checkKey('UI_RIGHT', 'just');
+		return checkKey('UI_RIGHT', JUST_PRESSED);
 
 	public var UI_DOWN_P(get, never):Bool;
 
 	inline function get_UI_DOWN_P()
-		return checkKey('UI_DOWN', 'just');
+		return checkKey('UI_DOWN', JUST_PRESSED);
 
 	public var UI_UP_R(get, never):Bool;
 
 	inline function get_UI_UP_R()
-		return checkKey('UI_UP', 'released');
+		return checkKey('UI_UP', RELEASED);
 
 	public var UI_LEFT_R(get, never):Bool;
 
 	inline function get_UI_LEFT_R()
-		return checkKey('UI_LEFT', 'released');
+		return checkKey('UI_LEFT', RELEASED);
 
 	public var UI_RIGHT_R(get, never):Bool;
 
 	inline function get_UI_RIGHT_R()
-		return checkKey('UI_RIGHT', 'released');
+		return checkKey('UI_RIGHT', RELEASED);
 
 	public var UI_DOWN_R(get, never):Bool;
 
 	inline function get_UI_DOWN_R()
-		return checkKey('UI_DOWN', 'released');
+		return checkKey('UI_DOWN', RELEASED);
 
 	// NOTES
 	public var NOTE_UP(get, never):Bool;
@@ -91,63 +91,63 @@ class Controls
 	public var NOTE_UP_P(get, never):Bool;
 
 	inline function get_NOTE_UP_P()
-		return checkKey('NOTE_UP', 'just');
+		return checkKey('NOTE_UP', JUST_PRESSED);
 
 	public var NOTE_LEFT_P(get, never):Bool;
 
 	inline function get_NOTE_LEFT_P()
-		return checkKey('NOTE_LEFT', 'just');
+		return checkKey('NOTE_LEFT', JUST_PRESSED);
 
 	public var NOTE_RIGHT_P(get, never):Bool;
 
 	inline function get_NOTE_RIGHT_P()
-		return checkKey('NOTE_RIGHT', 'just');
+		return checkKey('NOTE_RIGHT', JUST_PRESSED);
 
 	public var NOTE_DOWN_P(get, never):Bool;
 
 	inline function get_NOTE_DOWN_P()
-		return checkKey('NOTE_DOWN', 'just');
+		return checkKey('NOTE_DOWN', JUST_PRESSED);
 
 	public var NOTE_UP_R(get, never):Bool;
 
 	inline function get_NOTE_UP_R()
-		return checkKey('NOTE_UP', 'released');
+		return checkKey('NOTE_UP', RELEASED);
 
 	public var NOTE_LEFT_R(get, never):Bool;
 
 	inline function get_NOTE_LEFT_R()
-		return checkKey('NOTE_LEFT', 'released');
+		return checkKey('NOTE_LEFT', RELEASED);
 
 	public var NOTE_RIGHT_R(get, never):Bool;
 
 	inline function get_NOTE_RIGHT_R()
-		return checkKey('NOTE_RIGHT', 'released');
+		return checkKey('NOTE_RIGHT', RELEASED);
 
 	public var NOTE_DOWN_R(get, never):Bool;
 
 	inline function get_NOTE_DOWN_R()
-		return checkKey('NOTE_DOWN', 'released');
+		return checkKey('NOTE_DOWN', RELEASED);
 
 	// GAME/UI
 	public var ACCEPT(get, never):Bool;
 
 	inline function get_ACCEPT()
-		return checkKey('ACCEPT', 'just');
+		return checkKey('ACCEPT', JUST_PRESSED);
 
 	public var BACK(get, never):Bool;
 
 	inline function get_BACK()
-		return checkKey('BACK', 'just');
+		return checkKey('BACK', JUST_PRESSED);
 
 	public var PAUSE(get, never):Bool;
 
 	inline function get_PAUSE()
-		return checkKey('PAUSE', 'just');
+		return checkKey('PAUSE', JUST_PRESSED);
 
 	public var RESET(get, never):Bool;
 
 	inline function get_RESET()
-		return checkKey('RESET', 'just');
+		return checkKey('RESET', JUST_PRESSED);
 
 	#if CAN_CHEAT
 	public var CHEAT(get, never):Bool;
@@ -165,7 +165,7 @@ class Controls
 	var id:Int;
 	var controller:FlxGamepad = null;
 
-	public function checkKey(key:String, type:String = 'pressed'):Bool
+	public function checkKey(key:String, type:TypePressed = PRESSED):Bool
 	{
 		var checked:Bool = false;
 
@@ -176,11 +176,13 @@ class Controls
 
 		switch (type)
 		{
-			case 'just':
+			case JUST_PRESSED:
 				checked = FlxG.keys.anyJustPressed(keyArray);
-			case 'released':
+
+			case RELEASED:
 				checked = FlxG.keys.anyJustReleased(keyArray);
-			default:
+
+			case PRESSED:
 				checked = FlxG.keys.anyPressed(keyArray);
 		}
 
@@ -198,11 +200,13 @@ class Controls
 			{
 				checked = switch (type)
 				{
-					case 'just':
+					case JUST_PRESSED:
 						controller.anyJustPressed(buttonArray);
-					case 'released':
+
+					case RELEASED:
 						controller.anyJustReleased(buttonArray);
-					default:
+
+					case PRESSED:
 						controller.anyPressed(buttonArray);
 				}
 			}
@@ -214,4 +218,11 @@ class Controls
 
 		return checked;
 	}
+}
+
+enum TypePressed
+{
+	PRESSED;
+	JUST_PRESSED;
+	RELEASED;
 }
